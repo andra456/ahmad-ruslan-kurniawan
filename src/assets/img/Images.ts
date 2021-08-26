f33cb6ed8d1b5ecbef45dc628b1e7f7b2c4aed42
Ahmad Ruslan <ager102@gmail.com>
2021-08-26 22:48:43 +0700

    Adding asset & create style common & global

---
+++ src/assets/img/Images.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as Loader } from '../../assets/img/ico/loading.svg';
+export { ReactComponent as Cancel } from '../../assets/img/ico/cancel.svg';
+export { ReactComponent as Like } from '../../assets/img/ico/love.svg';
+export { ReactComponent as Logo } from '../../assets/img/logo/logo.svg';
+export { ReactComponent as Logo2 } from '../../assets/img/logo/logo2.svg';
+export { ReactComponent as Menu } from '../../assets/img/ico/menu.svg';
+export { ReactComponent as Gallery } from '../../assets/img/ico/gallery.svg';
